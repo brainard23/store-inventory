8ee1d1a5c24ddfc50ab0f7c44c5e9c93f5026a44
imakid143 <brainard.ordona@fligno.com>
2023-06-13 22:18:56 +0800

    update inventory page

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -2,8 +2,22 @@ import { Text, View } from "react-native";
 
 const Dashboard = () => {
   return (
-    <View className=" w-full h-52">
-      <Text>dashboard</Text>
+    <View className="w-full h-40">
+      <View className="flex-row justify-center items-center p-2">
+        <Text className="font-bold text-lg">Reports</Text>
+      </View>
+      <View className="flex-row justify-between items-center mx-2 border rounded-sm	p-5">
+        <View className="flex-column items-left w-[50%]">
+          <Text>Total Items: 12 </Text>
+          <Text>Total Sold Items: 51</Text>
+          <Text>Profit: Php 1000</Text>
+        </View>
+        <View className="flex-column items-left w-[50%]">
+            <Text>Out of Stocks</Text>
+            <Text>Out of Stocks</Text>
+            <Text>Out of Stocks</Text>
+        </View>
+      </View>
     </View>
   );
 };
--- page/Home.js
+++ page/Home.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
 import Dashboard from "../components/Dashboard";
+import { VirtualizedList } from "react-native";
 
 const Homescreen = ({ navigation }) => {
   const [hasPermission, setHasPermission] = useState(null);
@@ -46,14 +47,44 @@ const Homescreen = ({ navigation }) => {
   //       <Button title={'Allow Camera'} onPress={() => askForCameraPermission()} />
   //     </View>)
   // }
+  const getItem = (_data, index) => ({
+    id: Math.random().toString(12).substring(0),
+    title: `Item ${index + 1}`,
+    stocks: Math.floor(Math.random() * 100),
+  });
 
-  // Return the View
+  const getItemCount = (_data) => 50;
+
+  const Item = ({ title, stocks }) => (
+    <View className="flex-row justify-between items-center w-full bg-red-300 h-12 my-1 px-1">
+        <Text>{title}</Text>
+        <Text>Stocks: {stocks}</Text>
+        <Text>Sold: {stocks}</Text>
+        <Text>Total: {stocks}</Text>
+        <Button title="Edit" />
+    </View>
+  );
   return (
     <View className="flex-column justify-center items-center">
       <View className="flex-row bg-[#246EE9] h-10 w-[100%] justify-center items-center">
         <Text className="text-white font-bold text-lg">My Inventory</Text>
       </View>
       <Dashboard />
+      <View className="bg-red-500 w-full">
+        <Button title="Add Product" />
+      </View>
+      <VirtualizedList
+        style={{
+          width: "100%",
+          marginVertical: 10,
+          marginHorizontal: 20,
+        }}
+        initialNumToRender={4}
+        renderItem={({ item }) => <Item title={item.title} stocks={item.stocks} />}
+        keyExtractor={(item) => item.id}
+        getItemCount={getItemCount}
+        getItem={getItem}
+      />
     </View>
 
     // {!scanned && (

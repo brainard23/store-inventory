2d3a2cf487b6c2b7cbcba7a83fd8d661eeb2f4cc
imakid143 <brainard.ordona@fligno.com>
2023-06-13 20:46:08 +0800

    update

---
+++ components/Dashboard.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Dashboard = () => {
+  return (
+    <View className=" w-full h-52">
+      <Text>dashboard</Text>
+    </View>
+  );
+};
+
+export default Dashboard;
--- page/Home.js
+++ page/Home.js
@@ -1,53 +1,69 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { useIsFocused } from '@react-navigation/native';
-import { RNCamera } from 'react-native-camera';
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import Dashboard from "../components/Dashboard";
 
-const Home = ({ navigation }) => {
-  const [barcode, setBarcode] = useState(null);
-  const isFocused = useIsFocused();
+const Homescreen = ({ navigation }) => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [text, setText] = useState("Not yet scanned");
 
-  const handleBarcodeScan = ({ barcodes }) => {
-    if (barcodes.length > 0) {
-      setBarcode(barcodes[0].data);
-    }
+  const askForCameraPermission = () => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
   };
 
+  // Request Camera Permission
   useEffect(() => {
-    if (isFocused) {
-      setBarcode(null);
-    }
-  }, [isFocused]);
+    askForCameraPermission();
+  }, []);
 
+  // What happens when we scan the bar code
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    setText(data);
+    console.log("Type: " + type + "\nData: " + data);
+  };
+
+  // Check permissions and return the screens
+  //change later to null
+  // if (hasPermission === true) {
+  //   return (
+  //     <View className="flex-1 bg-red-500">
+  // <BarCodeScanner
+  //   onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+  //   style={{ height: 500, width: 500 }}
+  // />;
+  //       <Text className="text-green-500">Requesting for camera permission</Text>
+  //     </View>)
+  // }
+  // if (hasPermission === false) {
+  //   return (
+  //     <View>
+  //       <Text style={{ margin: 10 }}>No access to camera</Text>
+  //       <Button title={'Allow Camera'} onPress={() => askForCameraPermission()} />
+  //     </View>)
+  // }
+
+  // Return the View
   return (
-    <View style={styles.container}>
-      <RNCamera
-        style={styles.camera}
-        type={RNCamera.Constants.Type.back}
-        captureAudio={false}
-        onBarCodeRead={handleBarcodeScan}
-      />
-      {barcode && <Text style={styles.barcodeText}>{barcode}</Text>}
+    <View className="flex-column justify-center items-center">
+      <View className="flex-row bg-[#246EE9] h-10 w-[100%] justify-center items-center">
+        <Text className="text-white font-bold text-lg">My Inventory</Text>
+      </View>
+      <Dashboard />
     </View>
+
+    // {!scanned && (
+    //   <Button
+    //     title={"Add Product"}
+    //     onPress={() => setScanned(false)}
+    //     color="tomato"
+    //   />
+    // )}
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  camera: {
-    flex: 1,
-    width: '100%',
-  },
-  barcodeText: {
-    marginTop: 16,
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-});
-
-export default Home;
+export default Homescreen;
